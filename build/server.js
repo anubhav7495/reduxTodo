678d695355e540cd6659afb422c369732821e0f3
anubhav7495 <anubhav.worklinux@gmail.com>
2017-10-03 16:43:21 +0530

    Added connect-history-api-fallback to dev-server

--- build/server.js
+++ build/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const webpack = require('webpack');
+const history = require('connect-history-api-fallback');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./webpack.dev.js');
 
 const app = express();
 const compiler = webpack(config);
+
+app.use(history());
 
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath
